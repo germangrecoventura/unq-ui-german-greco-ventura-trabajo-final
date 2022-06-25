c1c1308236edf6787b9dfa68475deaca012e72db
germangrecoventura <germangv1998@gmail.com>
2022-06-25 12:46:31 -0300

    Agrego impresión de los elementos basicos del piedra-papel-tijera

--- piedra-papel-tijera-lagarto-spock/src/App.js
+++ piedra-papel-tijera-lagarto-spock/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import "slick-carousel/slick/slick.css"; 
+import logo from "./logo.svg";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Opcion from "./Opcion";
 
 function App() {
+  const opciones = [
+    {
+      eleccion: "piedra",
+      derrota: "tijera",
+    },
+    {
+      eleccion: "papel",
+      derrota: "piedra",
+    },
+    {
+      eleccion: "tijera",
+      derrota: "papel",
+    },
+  ];
   return (
     <div className="App">
+      <div className="opciones">
+        {opciones.map((e, index) => (
+          <Opcion valor={opciones[index]} />
+        ))}
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ piedra-papel-tijera-lagarto-spock/src/Opcion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Opcion = (props) => {
+  return <div className="opcion">{props.valor.eleccion}</div>;
+};
+
+export default Opcion
