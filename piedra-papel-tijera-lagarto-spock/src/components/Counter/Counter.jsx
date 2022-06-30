1e2eb665a1bdffc8f83f2430e123605376f6b695
germangrecoventura <germangv1998@gmail.com>
2022-06-30 18:35:29 -0300

    Arreglo importación de imagen

--- piedra-papel-tijera-lagarto-spock/src/components/Counter/Counter.jsx
+++ piedra-papel-tijera-lagarto-spock/src/components/Counter/Counter.jsx
@@ -3,13 +3,13 @@ import ia from "./brain.png"
 import user from "./user.png"
 
 const Counter = (props) => {
-  const player = props.isMachine ? {ia} :{user};
+  const player = props.isMachine ? ia :user;
 
   return (
     <div className="counter">
       <p>Games won by <img src={user} className="image-count"/>: {props.firstCount}</p>
       <p>
-        Games won by <img src={ia} className="image-count"/>: {props.secondCount}
+        Games won by <img src={player} className="image-count"/>: {props.secondCount}
       </p>
     </div>
   );
