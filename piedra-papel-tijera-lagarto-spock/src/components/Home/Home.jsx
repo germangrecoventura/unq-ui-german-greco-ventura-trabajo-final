d8aca54ab93d599f17172ef0b184e334bd5b11fa
germangrecoventura <germangv1998@gmail.com>
2022-06-30 14:53:37 -0300

    Agrego redireccionamiento a las distintas páginas

--- piedra-papel-tijera-lagarto-spock/src/components/Home/Home.jsx
+++ piedra-papel-tijera-lagarto-spock/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import onePlayer from "./images/OnePlayer.png";
 import twoPlayer from "./images/Two Player.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -11,8 +12,12 @@ function Home() {
           <h1> Rock, Paper, Scissors, Lizard, Spock</h1>
           <h2> Select your game mode</h2>
           <div className="selector-games">
-            <img src={onePlayer} className="image-game" />
-            <img src={twoPlayer} className="image-game" />
+            <Link to="/one-player">
+              <img src={onePlayer} alt="Player VS IA" className="image-game" />
+            </Link>
+            <Link to="/two-players" >
+              <img src={twoPlayer} alt="Player VS Player" className="image-game" />
+            </Link>
           </div>
         </div>
       </div>
